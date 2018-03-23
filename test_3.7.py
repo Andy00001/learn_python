c511f65046b3129c11ae05f3c5af9022d1cf1591
Andy00001 <1141397038@qq.com>
2018-03-23 09:54:17 +0800

    add test_3.7.py

---
+++ test_3.7.py
@@ -0,0 +1,13 @@
+#!/bin/python
+x = "there is %d apples" % 10
+binary = "binary"
+do_not = "don't"
+
+y = "those who know %s and those who %s" %(binary,do_not)
+
+print x,y
+print "i said : %r" % x 
+print "i also said :%r" %y
+
+
+
